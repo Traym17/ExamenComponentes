89238d37dcdd2b6c080e534d15d219a8a55e72ec
Traym17 <51390112+Traym17@users.noreply.github.com>
2020-10-17 10:31:35 -0600

    Examen componentes1

---
+++ ExamenComponentes1/ExamenComponentes1/UserControl1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace ExamenComponentes1
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label27 = new System.Windows.Forms.Label();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(255, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 25);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Iniciar juego";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox5);
+            this.panel3.Controls.Add(this.label25);
+            this.panel3.Controls.Add(this.label24);
+            this.panel3.Controls.Add(this.label23);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.textBox4);
+            this.panel3.Controls.Add(this.label22);
+            this.panel3.Controls.Add(this.label21);
+            this.panel3.Controls.Add(this.label20);
+            this.panel3.Controls.Add(this.label19);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Location = new System.Drawing.Point(1, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(716, 340);
+            this.panel3.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(357, 202);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 16;
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Location = new System.Drawing.Point(457, 54);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(13, 13);
+            this.label25.TabIndex = 15;
+            this.label25.Text = "0";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Location = new System.Drawing.Point(582, 56);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(13, 13);
+            this.label24.TabIndex = 14;
+            this.label24.Text = "0";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(320, 54);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(13, 13);
+            this.label23.TabIndex = 13;
+            this.label23.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(357, 148);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Comparar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(357, 105);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(250, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(291, 104);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(59, 19);
+            this.label22.TabIndex = 10;
+            this.label22.Text = "Codigo";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(430, 14);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(61, 19);
+            this.label21.TabIndex = 9;
+            this.label21.Text = "Totales";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(566, 14);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(41, 19);
+            this.label20.TabIndex = 8;
+            this.label20.Text = "Vida";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(291, 14);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(76, 19);
+            this.label19.TabIndex = 7;
+            this.label19.Text = "Parciales";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label18);
+            this.panel4.Controls.Add(this.label17);
+            this.panel4.Controls.Add(this.label16);
+            this.panel4.Controls.Add(this.label15);
+            this.panel4.Controls.Add(this.label14);
+            this.panel4.Controls.Add(this.label13);
+            this.panel4.Controls.Add(this.label12);
+            this.panel4.Location = new System.Drawing.Point(16, 14);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(203, 140);
+            this.panel4.TabIndex = 0;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(104, 40);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(54, 15);
+            this.label18.TabIndex = 6;
+            this.label18.Text = "Verde: V";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(15, 65);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(46, 15);
+            this.label17.TabIndex = 5;
+            this.label17.Text = "Rojo: R";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(15, 94);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(70, 15);
+            this.label16.TabIndex = 4;
+            this.label16.Text = "Amarillo: M";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(104, 94);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(60, 15);
+            this.label15.TabIndex = 3;
+            this.label15.Text = "Blanco: B";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(104, 66);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(56, 15);
+            this.label14.TabIndex = 2;
+            this.label14.Text = "Negro: N";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(15, 40);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(45, 15);
+            this.label13.TabIndex = 1;
+            this.label13.Text = "Azul: A";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(25, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(150, 19);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "Colores disponibles";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label26.Location = new System.Drawing.Point(252, 106);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(48, 15);
+            this.label26.TabIndex = 2;
+            this.label26.Text = "label26";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(323, 106);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(159, 20);
+            this.textBox6.TabIndex = 3;
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label27.Location = new System.Drawing.Point(301, 64);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(105, 15);
+            this.label27.TabIndex = 4;
+            this.label27.Text = "¿Jugar de nuevo?";
+            // 
+            // UserControl1
+            // 
+            this.Controls.Add(this.label27);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label26);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button2);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(716, 340);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_iniciar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label intentos;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label47;
+        private System.Windows.Forms.Label label34;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label27;
+    }
+}
---
+++ ExamenComponentes1/ExamenComponentes1/UserControl1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenComponentes1
+{
+    public partial class UserControl1: UserControl
+    {
+        int contIntenton = 8;
+        string colores = "ARBNVM";
+        char []codigoRandom=new char[4];
+        public UserControl1()
+        {
+            InitializeComponent();
+            panel3.Visible = false;
+            textBox6.Visible = false;
+            label26.Visible = false;
+            label27.Visible = false;
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            panel3.Visible = true;
+            generarCodigo();
+        }
+        public void generarCodigo()
+        {
+            var random = new Random();
+            for (var i = 0; i < codigoRandom.Length; i++)
+            {
+                codigoRandom[i] = colores[random.Next(0, colores.Length)];
+            }
+            //textBox5.Text = codigoRandom[0] + " " + codigoRandom[1]
+                //+" "+ codigoRandom[2]+" "+ codigoRandom[3];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox4.TextLength> 4)
+            {
+                textBox5.Text = "Digite solo 4 letras";
+            }
+            else
+            {
+                textBox4.CharacterCasing = CharacterCasing.Upper;
+                char[] cIngresado = textBox4.Text.ToCharArray();
+                int coincidencias = 0;
+                int totales = 0;
+                if (contIntenton == 0)
+                {
+                    textBox5.Text = "Lo siento tus intentos se acabaron :(";
+                    volverJugar("Perdiste");
+                }
+                else
+                {
+                    //pego el codigo
+                    if (cIngresado.SequenceEqual(codigoRandom))
+                    {
+                        textBox5.Text = "Ganaste";
+                        volverJugar("Ganaste");
+                    }
+                    //tal vez posicion o nada :(
+                    else
+                    {
+                        coincidencias = cIngresado.Intersect(codigoRandom).Count();
+                        textBox5.Text = "Todavia no :(";
+                        contIntenton--;
+                        label23.Text = coincidencias.ToString();
+                        label24.Text = contIntenton.ToString();
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (cIngresado[i] == codigoRandom[i])
+                            {
+                                totales++;
+                            }
+                        }
+                        label25.Text = totales.ToString();
+                    }
+                } 
+            }
+
+
+         }
+        private void volverJugar(String mensaje)
+        {
+            panel3.Visible = false;
+            button2.Visible = true;
+            textBox6.Visible = true;
+            label26.Visible = true;
+            label27.Visible = true;
+            contIntenton = 8;
+            label23.Text = " "; 
+            label25.Text = " ";
+            label24.Text = " ";
+            textBox4.Text = " ";
+            textBox6.Text = codigoRandom[0] + " " + codigoRandom[1]
+                + " " + codigoRandom[2] + " " + codigoRandom[3];
+            label26.Text = mensaje;
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
